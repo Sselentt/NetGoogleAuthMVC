374f64d36d36510143ddf824d347d32714fe5d7b
Sselentt <sselentt23@gmail.com>
2024-01-27 01:54:40 +0300

    Add project files.

---
+++ NetGoogleAuthMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetGoogleAuthMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
